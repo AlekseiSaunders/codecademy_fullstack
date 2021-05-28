d5ba249280e572f4211e8b07f00bf100d2be4c50
AlekseiSaunders <alekseisaunders@gmail.com>
2021-05-28 15:31:04 -0600

    created randomization code for data

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 const button = document.querySelector("button");
+const getData = function () {
+  return axios.get("/meditation.json");
+};
 
 button.addEventListener("click", function (e) {
   e.preventDefault();
   addQuote();
   addMeditation();
   addTime();
-  /* let quoteNum = res.data.quotes.length - 1;
-  let medNum = res.data.meditation.length - 1;
-  let lengthNum = res.data.medLength.length - 1;*/
 });
 
 const addQuote = async function () {
   const quoteDiv = document.getElementById("quote");
   const res = await getData();
-  quoteDiv.append(res.data.quotes);
+  let quoteNum = res.data.quotes.length - 1;
+  let quoteIndex = Math.floor(Math.random() * quoteNum);
+  quoteDiv.append(res.data.quotes[quoteIndex]);
 };
 const addMeditation = async function () {
   const meditationDiv = document.getElementById("meditation");
   const res = await getData();
-  meditationDiv.append(res.data.meditation);
+  let medNum = res.data.meditation.length - 1;
+  let meditationIndex = Math.floor(Math.random() * medNum);
+  meditationDiv.append(res.data.meditation[meditationIndex]);
 };
 const addTime = async function () {
   const timeDiv = document.getElementById("time");
   const res = await getData();
-  timeDiv.append(res.data.medLength);
-};
-const getData = function () {
-  return axios.get("/meditation.json");
+  let lengthNum = res.data.medLength.length - 1;
+  let lengthIndex = Math.floor(Math.random() * lengthNum);
+  timeDiv.append(res.data.medLength[lengthIndex]);
 };
